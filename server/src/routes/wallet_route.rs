38c546a2f144bb161a33596eb6aa4e1d8fc2337d
oyindamola oladapo <oyinoladapo5@gmail.com>
2025-06-09 05:15:30 +0100

    fix route path error

--- server/src/routes/wallet_route.rs
+++ server/src/routes/wallet_route.rs
@@ -8,7 +8,7 @@ use std::collections::HashMap;
 pub fn wallet_router() -> Router {
     Router::new()
     .route("/wallets", post(create_wallet_handler))
-    .route("x", get(get_new_address))
+    .route("/wallets/:ids/address", get(get_new_address))
     .with_state(AppState {
         wallet_store: Arc::new(Mutex::new(HashMap::new())),
 })
